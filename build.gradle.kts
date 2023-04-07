fe037b7f278589e91f85376eabfb32e8b1cfa88c
Peter Chege <peterkagure@gmail.com>
2023-04-07 17:36:09 +0300

    Upgrade AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.dokka.DokkaDefaults.format
-import org.jetbrains.kotlin.builtins.StandardNames.FqNames.target
-
 plugins {
     id("com.diffplug.spotless") version "5.3.0"
 }
@@ -11,7 +8,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.2.2")
+        classpath ("com.android.tools.build:gradle:7.4.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
         classpath ("com.google.dagger:hilt-android-gradle-plugin:2.45")
         classpath ("com.google.gms:google-services:4.3.15")
