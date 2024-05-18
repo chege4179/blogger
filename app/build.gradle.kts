ea547417aae10d22ce8bb386fc107e051c375d6e
Peter Chege <peterkagure@gmail.com>
2024-05-18 16:51:42 +0300

    Update verison code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.peterchege.blogger"
         minSdk = 21
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
