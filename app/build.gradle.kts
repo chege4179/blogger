3e30c575bac2811954a5a6f23466836406ccb817
Peter Chege <peterkagure@gmail.com>
2024-09-22 22:09:12 +0300

    Update upload artifact

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.peterchege.blogger"
         minSdk = 21
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
